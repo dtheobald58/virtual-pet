2e0f45d55cf4c16d084b96337115604067b91cb7
dtheobald58 <dtheobald58@yahoo.com>
2016-09-16 14:45:45 -0400

    initial commit

---
+++ Stats.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPetTheobald
+{
+    class Stats
+    {
+        private string name;
+        private int hunger;
+        private int boredom;
+    }
+}
