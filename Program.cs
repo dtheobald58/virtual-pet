6b257cad726788862f9889c692bb312db4e34d6b
dtheobald58 <dtheobald58@yahoo.com>
2016-09-19 21:32:48 -0400

    working now

--- Program.cs
+++ Program.cs
@@ -10,8 +10,27 @@ namespace VirtualPetTheobald
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("|");
-            Console.WriteLine("0");
+            Stats worm = new Stats();
+            int userNum = 0;
+            worm.Portrait();
+            worm.Start();
+
+            Console.Clear();
+            worm.Portrait();
+            Console.Clear();
+            Console.Write("I hope you didn't think it would be easy to care for a pet worm.\n" + worm.Name + " looks like a handful (and a monster).\n\n");
+            
+            do
+            {
+                worm.Portrait();
+                worm.DisplayStats();
+                worm.Interact(userNum);
+                Console.Clear();
+            } while (worm.Hunger > 0 && worm.Thirst > 0 && worm.Happiness > 0);
+
+            Console.WriteLine("I've got some bad news... " + worm.Name + " was drafted into the worm army.");
+            Console.WriteLine();
+            Console.Write("Don't try to write any letters, " + worm.Name + " won't write back,\nbecause " + worm.Name + " is a worm, and worms don't have arms.\n\nBut they do have wars.\n\n\n\n");
         }
     }
 }
--- Stats.cs
+++ Stats.cs
@@ -8,8 +8,113 @@ namespace VirtualPetTheobald
 {
     class Stats
     {
+        //fields
         private string name;
-        private int hunger;
-        private int boredom;
+        private int hunger = 10;
+        private int happiness = 10;
+        private int thirst = 10;
+
+        //properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Happiness
+        {
+            get { return this.happiness; }
+            set { this.happiness = value; }
+        }
+
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+
+        public int Thirst
+        {
+            get { return this.thirst; }
+            set { this.thirst = value; }
+        }
+
+        //constructors
+        public Stats()
+        {
+         
+        }
+
+        public Stats (string Name)
+        {
+            this.Name = Name;
+        }
+
+        //methods
+        public string Start ()
+        {
+            Console.Write("Congratulations on your new pet worm!!!  It's... cute... I guess.\nWhat's its name?");
+            Console.WriteLine();
+            Console.WriteLine("(Please enter the name of your pet)");
+            Name = Console.ReadLine();
+
+            return this.Name;
+        }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine("HAPPINESS: [" + happiness + "]");
+            Console.WriteLine("HUNGER: [" + hunger + "]");
+            Console.WriteLine("THIRST: [" + thirst + "]");
+        }
+
+        public void Interact(int userInput)
+        {
+            Console.WriteLine("What would you like to do with " + name + "?");
+            Console.WriteLine();
+            Console.Write("Enter \"1\" to feed " + name + ". \nEnter \"2\" to play with " + name + ". \nEnter \"3\" to quench " + name + "'s thirst.\n");
+            userInput = int.Parse(Console.ReadLine());
+            if (userInput == 1)
+            {
+                Hunger++;
+                Thirst--;
+                Thirst--;
+                Happiness++;
+            }
+            else if (userInput == 2)
+            {
+                Happiness++;
+                Hunger--;
+                Thirst--;
+            }
+            else if (userInput == 3)
+            {
+                Thirst++;
+                Hunger--;
+                Hunger--;
+                Happiness++;
+            }
+            else
+            {
+                Thirst--;
+                Hunger--;
+                Happiness--;
+            }
+        }
+
+        public void Portrait ()
+        {
+            Console.WriteLine("                           _____");
+            Console.WriteLine("                          / o  o\\");
+            Console.WriteLine("                          |  )-(|");
+            Console.WriteLine("                          |     |");
+            Console.WriteLine("           ____           |     |");
+            Console.WriteLine("          / _  \\         /     /");
+            Console.WriteLine("         / / \\  \\_______/     /");
+            Console.WriteLine("     ___/ /   \\              /");
+            Console.WriteLine("    (____/     \\____________/");
+        }
+
+
     }
 }
